f486bff29a01ba8422864567c55d7069c4b7762a
this-is-me-123 <my.personal.email.cv@gmail.com>
2025-06-12 18:29:15 -0500

    Add chatRouter utility and update test path

--- 02_ai_chat_persona/tests/persona_style.test.js
+++ 02_ai_chat_persona/tests/persona_style.test.js
@@ -4,7 +4,7 @@
  * Tests that the chat persona responds with the correct tone.
  */
 const assert = require('assert');
-const { generateResponse } = require('../training_scripts/chatRouter');
+const { generateResponse } = require('../../training_scripts/chatRouter');
 
 describe('Chat Persona Style', () => {
   it('should use friendly tone for a greeting', async () => {
