767174bb6dc345cff42b24d093e16e4dc65429bb
this-is-me-123 <my.personal.email.cv@gmail.com>
2025-06-21 20:10:30 -0400

    Activate venv in test scripts

--- run_all_tests.bat
+++ run_all_tests.bat
@@ -1,3 +1,7 @@
+@echo off
+if exist .venv\Scripts\activate.bat (
+    call .venv\Scripts\activate.bat
+)
 pytest -q
 npm test --silent
 
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+set -e
+if [ -f ".venv/bin/activate" ]; then
+    source .venv/bin/activate
+fi
 pytest -q
 npm test --silent
